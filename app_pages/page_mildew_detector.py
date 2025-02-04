12c22718d9d3a54cdf3fae3fe30fc859641de8f1
Godwin C. Naces <157016869+GNaces@users.noreply.github.com>
2025-02-04 02:12:02 +0000

    dataframe append() removed, updated with .concat()

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -59,8 +59,7 @@ def make_live_predict(images_buffer):
                 resized_img, version=version)
             plot_predictions_probabilities(pred_proba, pred_class)
 
-            df_report = df_report.append({"Name": image.name, 'Result': pred_class},
-                                         ignore_index=True)
+            df_report = pd.concat([df_report, pd.DataFrame([{"Name": image.name, "Result": pred_class}])], ignore_index=True)
 
         if not df_report.empty:
             st.success("Analysis Report")
